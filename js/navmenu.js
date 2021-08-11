9deb1e27c61bb835db9bcd200937f9eb8df46dd1
Tyler <tyler.thompson@tufts.edu>
2021-08-11 16:37:56 -0700

    initial mobile navbar

---
+++ js/navmenu.js
@@ -0,0 +1,8 @@
+function openNavMenu() {
+    var navMenu = document.getElementById("navmenu");
+    navMenu.style.width = "85vw";
+}
+function closeNavMenu() {
+    var navMenu = document.getElementById("navmenu");
+    navMenu.style.width = "0";
+}
