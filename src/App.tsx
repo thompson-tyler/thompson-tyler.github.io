8302c6ce0edb38240db915e871d6d614ab2e5e51
Tyler Thompson <tyler.thompson@tufts.edu>
2022-11-03 13:40:50 -0400

    Scroll to top of page on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Navbar from "./navbar";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "./components/scroll-to-top";
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
     </div>
---
+++ src/components/scroll-to-top.tsx
@@ -0,0 +1,17 @@
+// Helper component to scroll to top of page on route change
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  }, [pathname]);
+
+  return null;
+}
